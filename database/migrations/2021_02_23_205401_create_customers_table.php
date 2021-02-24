cff16b27948b999b0f72d9702b03f4d231fd311a
EKUE-HETTAH Boris <kueviboris@gmail.com>
2021-02-24 06:22:49 +0000

    Create migrations files

---
+++ database/migrations/2021_02_23_205401_create_customers_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCustomersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('firstname');
+            $table->string('lastname');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->timestamps();
+        });
+
+        // Insert data into 'Customers' table
+        DB::table('customers')->insert(
+            array(
+                'firstname' => 'Boris',
+                'lastname' => 'EKUE-HETTAH',
+                'email' => 'kueviboris@gmail.com',
+                'password' => 'admin'
+            )
+        );
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('customers');
+    }
+}
---
+++ database/migrations/2021_02_23_211139_create_tokens_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTokensTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tokens', function (Blueprint $table) {
+            $table->id();
+            $table->string('access_token')->unique();;
+            $table->timestamps('expires_at');
+            $table->string('token_type');
+            $table->string('id_user');
+           // $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tokens');
+    }
+}
